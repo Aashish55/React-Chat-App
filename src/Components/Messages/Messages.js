07cfcb4738b4155cbb25670b1cf4306ba3d47cac
Aashish Jangam <38790510+Aashish55@users.noreply.github.com>
2020-08-02 07:04:10 +0545

    chat search completed

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -13,7 +13,10 @@ class Messages extends React.Component {
     user: this.props.currentUser,
     messages: [],
     messageLoading: true,
-    numUniqueUsers:''
+    numUniqueUsers: '',
+    searchTerm: '',
+    searchLoading: false,
+    searchResults: []
   }
 
   componentDidMount() {
@@ -41,22 +44,52 @@ class Messages extends React.Component {
   }
 
   countUniqueUsers = messages => {
-    const uniqueUsers = messages.reduce((acc,message)=>{
-      if(!acc.includes(message.user.name)){
+    const uniqueUsers = messages.reduce((acc, message) => {
+      if (!acc.includes(message.user.name)) {
         acc.push(message.user.name)
       }
       return acc;
-    },[])
-    const plural = uniqueUsers.length>1 || uniqueUsers.length===0;
-    const numUniqueUsers = `${uniqueUsers.length} user${plural?'s':''}`;
-    this.setState({numUniqueUsers})
+    }, [])
+    const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
+    const numUniqueUsers = `${uniqueUsers.length} user${plural ? 's' : ''}`;
+    this.setState({ numUniqueUsers })
   }
 
   displayChannelName = channel => channel ? `# ${channel.name}` : '';
 
+  handleSearchChange = event => {
+    this.setState({
+      searchTerm: event.target.value,
+      searchLoading: true
+    }, () => this.handleSearchMessages())
+  }
+
+  handleSearchMessages = () => {
+    const channelMessages = [...this.state.messages];
+    const regex = new RegExp(this.state.searchTerm, 'gi');
+    const searchResults = channelMessages.reduce((acc, message) => {
+      if (message.content && (message.content.match(regex) || message.user.name.match(regex))) {
+        acc.push(message)
+      }
+      return acc;
+    }, [])
+    this.setState({ searchResults })
+    setTimeout(()=>this.setState({searchLoading:false}),1000)
+  }
+
+  displayMessages = messages => (
+    messages.length > 0 && messages.map(message => (
+      <MessageComponent
+        key={message.timestamp}
+        message={message}
+        user={this.state.user}
+      />
+    ))
+  )
+
   render() {
 
-    const { messagesRef, messages, channel, user, numUniqueUsers } = this.state
+    const { messagesRef, messages, channel, user, numUniqueUsers, searchTerm,searchResults,searchLoading } = this.state
 
     return (
       <React.Fragment>
@@ -64,16 +97,13 @@ class Messages extends React.Component {
         <MessagesHeader
           channelName={this.displayChannelName(channel)}
           numUniqueUsers={numUniqueUsers}
+          handleSearchChange={this.handleSearchChange}
+          searchLoading={searchLoading}
         />
 
         <Segment className='chatbox'>
           <Comment.Group className='messages'>
-            {messages.length > 0 && messages.map(message =>
-              <MessageComponent
-                key={message.timestamp}
-                message={message}
-                user={user}
-              />)}
+            {searchTerm ? this.displayMessages(searchResults) : this.displayMessages(messages)}
           </Comment.Group>
         </Segment>
 
--- src/Components/Messages/MessagesHeader.js
+++ src/Components/Messages/MessagesHeader.js
@@ -4,21 +4,27 @@ import { Header, Segment, Input, Icon } from 'semantic-ui-react'
 class MessagesHeader extends Component {
     state = {}
     render() {
-        const {channelName,numUniqueUsers} = this.props
-    
+        const { channelName, numUniqueUsers, handleSearchChange,searchLoading } = this.props
+
         return (
             <Segment clearing>
 
                 <Header fluid='true' as='h2' floated='left' style={{ marginBottom: 0 }}>
                     <span>
                         {channelName}
-            <Icon name='star outline' color='black' />
+                        <Icon name='star outline' color='black' />
                     </span>
                     <Header.Subheader>{numUniqueUsers}</Header.Subheader>
 
                 </Header>
                 <Header floated='right'>
-                    <Input icon='search' size='mini' name='searchTerm' placeholder='Search Messages' />
+                    <Input 
+                        icon='search'
+                        loading={searchLoading}
+                        onChange={handleSearchChange}
+                        size='mini'
+                        name='searchTerm'
+                        placeholder='Search Messages' />
                 </Header>
             </Segment>
         );
