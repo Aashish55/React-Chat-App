95fb26fa73d1a448a3a1247157f237b09bd98638
Aashish Jangam <38790510+Aashish55@users.noreply.github.com>
2020-08-06 06:19:01 +0545

    starred channels created

--- src/Components/Sidepanel/SidePanel.js
+++ src/Components/Sidepanel/SidePanel.js
@@ -3,6 +3,7 @@ import UserPanel from "./UserPanel";
 import Channels from "./Channels";
 import { Menu } from "semantic-ui-react";
 import DirectMessages from './DirectMessages'
+import Starred from './Starred'
 
 class SidePanel extends React.Component {
   render() {
@@ -17,6 +18,7 @@ class SidePanel extends React.Component {
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
         <UserPanel currentUser={currentUser} />
+        <Starred />
         <Channels currentUser={currentUser} />
         <DirectMessages currentUser={currentUser} />
       </Menu>
