7d15493312298c26a78dfad64fc2a18f259c974c
Aashish Jangam <38790510+Aashish55@users.noreply.github.com>
2020-08-16 06:39:33 +0545

    Color Pickers added

--- src/Components/ColorPanel/ColorPanel.js
+++ src/Components/ColorPanel/ColorPanel.js
@@ -1,8 +1,18 @@
 import React from "react";
-import {Sidebar,Menu,Divider,Button} from 'semantic-ui-react'
+import { Sidebar, Menu, Divider, Button, Modal, Icon } from 'semantic-ui-react';
+import {CirclePicker} from 'react-color'
 
 class ColorPanel extends React.Component {
+  state = {
+    modal: false
+  }
+
+  openModal = () => this.setState({ modal: true });
+
+  closeModal = () => this.setState({ modal: false })
+
   render() {
+    const { modal } = this.state;
     return (
       <Sidebar
         as={Menu}
@@ -12,9 +22,24 @@ class ColorPanel extends React.Component {
         visible
         width='very thin'
       >
-      <Divider />
-      <Button icon='add' size='small' color='blue' />
-        
+        <Divider />
+        <Button icon='add' size='small' color='blue' onClick={this.openModal} />
+
+        <Modal basic open={modal} onClose={this.closeModal} >
+          <Modal.Header>Choose App Colors</Modal.Header>
+          <Modal.Content>
+            <CirclePicker />
+          </Modal.Content>
+          <Modal.Actions>
+            <Button inverted color='green'>
+              <Icon name='checkmark' /> Save Colors
+            </Button>
+            <Button inverted color='red' onClick={this.closeModal}>
+              <Icon name='remove' /> Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
+
       </Sidebar>
     );
   }
